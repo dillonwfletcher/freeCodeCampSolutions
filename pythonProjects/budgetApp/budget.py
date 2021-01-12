23f4257bc5c560e3b682a2a9ca76a4d3554dd690
dillonwfletcher <dillon.w.fletcher@gmail.com>
2021-01-12 08:54:10 -0500

    finished graph function. submitting final sln to budget app pr project

--- pythonProjects/budgetApp/budget.py
+++ pythonProjects/budgetApp/budget.py
@@ -1,14 +1,11 @@
 class Category:
      
-    name = ""
-
     def __init__(self, name):
         self.name = name
         self.ledger = []
         self.balance = 0
 
     def __str__(self):
-        
         title = "*" * ((30-len(self.name))//2) + self.name + "*" * ((30-len(self.name))//2) 
         for item in self.ledger:
             left = item['description'][:23]
@@ -19,13 +16,10 @@ class Category:
         return title
 
     def deposit(self, amount, description = ""):
-
         self.ledger.append({"amount": amount, "description": description})
         self.balance += amount
 
-
     def withdraw(self, amount, description = ""):
-
         if not self.check_funds(amount):
             return False
 
@@ -35,11 +29,9 @@ class Category:
         return True
 
     def get_balance(self):
-        
         return self.balance
 
     def transfer(self, amount, category):
-        
         if not self.check_funds(amount):
             return False
 
@@ -49,41 +41,67 @@ class Category:
         return True
 
     def check_funds(self, amount):
-
         if amount > self.balance:
             return False
-
         return True
-
 
 def create_spend_chart(categories):
     
-    title = "Percentage spent by category"
-    print(title)
-
-    # creates y axis
-    for i in range(100, -1, -10):
-        print(" " * (3-len(str(i))), i, "|", sep="")
-    
-    #todo: add in bar chart things
-
-
-    # creates x axis line
-    print(" "*4 + "-"*10)
-   
-    # creates the x axis labels
     arr = []
     max_cat = 0
+    spent_d = {"total":0}
+
     for item in categories:
         arr.append(list(item.name))
         if len(list(item.name)) > max_cat:
             max_cat = len(list(item.name))
+        
+        spent = 0
+        for i in range(len(item.ledger)):
+            amount = item.ledger[i]["amount"]
+            if amount < 0:
+                spent += amount
 
+        spent_d[item.name] = round(spent, 2)
+        spent_d["total"] += round(spent, 2)
+
+    # adds spaces to end of list holding characters of category name, so that all lists are of same length when printing names vertically    
     for a in arr:
         if len(a) != max_cat:
             for x in range(max_cat - len(a)):
                 a.append(" ")
 
+    # printing graph
+    graph = "Percentage spent by category\n"
+
+    for i in range(100, -1, -10):
+        line = " " * (3-len(str(i))) + str(i) + "| "
+
+        for item in categories:
+            per = round(spent_d[item.name] / spent_d["total"], 2) * 100
+
+            if i <= per: line += "o" + " "*2
+            else: line += " "*3
+
+        graph += line + "\n"
+    
+    # each bar takes up --- so print 3 * # of categories + 1 to make dashed line go -- past last bar
+    graph += " "*4 + "-"*(3*len(categories) + 1) + "\n"
+
+    # prints the category names vertically on x axis
     for x, y, z in zip(*arr):
-        print(" " * 5, end="")
-        print(x,y,z, sep =" "*2)
+        graph += " "*5
+        graph += x + " "*2 + y + " "*2 + z + " "*2 + "\n"
+
+    # return string that will print graph minus newline at end
+    return graph[:-1]
+
+
+
+
+
+
+
+
+
+
